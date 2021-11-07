7adf0fd3fe7c827220ab2e10135650bb8eb9466c
cubetoon <mail@pettersnet.de>
2021-11-07 20:08:21 +0100

    creating es8388 skeleton

---
+++ targets/esp32/main/sinks/ES8388AudioSink.h
@@ -0,0 +1,22 @@
+#ifndef ES8388AUDIOSINK_H
+#define ES8388AUDIOSINK_H
+
+#include <vector>
+#include <iostream>
+#include "BufferedAudioSink.h"
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_log.h"
+
+class ES8388AudioSink : public BufferedAudioSink
+{
+public:
+    ES8388AudioSink();
+    ~ES8388AudioSink();
+private:
+};
+
+#endif
